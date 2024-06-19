bb4474a96c3d91bdd9686297e6ae6022f4d69b7a
unknown <jonatanesprellalopez@gmail.com>
2024-06-19 10:15:01 -0400

    Se realizo la refactorizacion del footer para una mejor administracion

--- src/componets/Footer_Main.jsx
+++ src/componets/Footer_Main.jsx
@@ -4,39 +4,77 @@ import { ReactComponent as IG } from "../assets/images/instagram.svg";
 import { ReactComponent as Spotify } from "../assets/images/instagram.svg";
 import { ReactComponent as Twitter } from "../assets/images/instagram.svg";
 import { ReactComponent as YouTube } from "../assets/images/instagram.svg";
-import "../styles/home_styles.css"
+import "../styles/Footer/general-footer.css";
+
+const SocialLink = ({ href, icon: Icon, children }) => (
+    <a href={href} className="social-link">
+        <Icon className="social-icon" /> {children}
+    </a>
+);
+
+const FooterColumn = ({ title, links }) => (
+    <div className="footer-column">
+        <h4>{title}</h4>
+        {links.map((link, index) => (
+            <a href={link.href} key={index}>
+                {link.text}
+            </a>
+        ))}
+    </div>
+);
+
 function FooterMain() {
+    const socialLinks = [
+        { href: "#", icon: Fb, text: "FACEBOOK" },
+        { href: "#", icon: IG, text: "INSTAGRAM" },
+        { href: "#", icon: Spotify, text: "SPOTIFY" },
+        { href: "#", icon: Twitter, text: "TWITTER" },
+        { href: "#", icon: YouTube, text: "YOUTUBE" }
+    ];
+
+    const footerColumns = [
+        {
+            title: "CEC - FGI",
+            links: [
+                { href: "#", text: "Quienes somos" },
+                { href: "#", text: "Hable con nosotros" },
+                { href: "#", text: "Aviso de Privacidad" }
+            ]
+        },
+        {
+            title: "Planes",
+            links: [
+                { href: "#", text: "Planes" },
+                { href: "#", text: "Documentos" }
+            ]
+        },
+        {
+            title: "Nuestra Compañía",
+            links: [
+                { href: "#", text: "Quiero ser franquiciatario" },
+                { href: "#", text: "Promociona tu marca" },
+                { href: "#", text: "Indica un local" },
+                { href: "#", text: "Trabaja con nosotros" }
+            ]
+        }
+    ];
+
     return (
         <footer className="footer">
             <div className="footer-top">
                 <h3 className="footer-title">Síguenos</h3>
                 <div className="footer-social-links">
-                    <a href="#" className="social-link"><Fb className="social-icon" /> FACEBOOK</a>
-                    <a href="#" className="social-link"><IG className="social-icon" /> INSTAGRAM</a>
-                    <a href="#" className="social-link"><Spotify className="social-icon" /> SPOTIFY</a>
-                    <a href="#" className="social-link"><Twitter className="social-icon" /> TWITTER</a>
-                    <a href="#" className="social-link"><YouTube className="social-icon" /> YOUTUBE</a>
+                    {socialLinks.map((link, index) => (
+                        <SocialLink href={link.href} icon={link.icon} key={index}>
+                            {link.text}
+                        </SocialLink>
+                    ))}
                 </div>
             </div>
             <div className="footer-bottom">
-                <div className="footer-column">
-                    <h4>CEC - FGI</h4>
-                    <a href="#">Quienes somos</a>
-                    <a href="#">Hable con nosotros</a>
-                    <a href="#">Aviso de Privacidad</a>
-                </div>
-                <div className="footer-column">
-                    <h4>Planes</h4>
-                    <a href="#">Planes</a>
-                    <a href="#">Documentos</a>
-                </div>
-                <div className="footer-column">
-                    <h4>Nuestra Compañía</h4>
-                    <a href="#">Quiero ser franquiciatario</a>
-                    <a href="#">Promociona tu marca</a>
-                    <a href="#">Indica un local</a>
-                    <a href="#">Trabaja con nosotros</a>
-                </div>
+                {footerColumns.map((column, index) => (
+                    <FooterColumn title={column.title} links={column.links} key={index} />
+                ))}
             </div>
         </footer>
     );
