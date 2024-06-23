c15be98e7acc8ace00c174e4ae72253dcf1b4d55
unknown <jonatanesprellalopez@gmail.com>
2024-06-22 21:24:30 -0400

    Se realizo la implementacion de  los documentos de entrenamiento

--- src/views/Rutinas.jsx
+++ src/views/Rutinas.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../styles/Rutinas.css';
+import "../styles/Rutinas/Training.css"
 
 import Header from "../componets/Header_Main"
 import Footer from "../componets/Footer_Main"
 
-import Imagen_C1 from "../assets/images/Rutinas/ImagenC1.png"
-import Imagen_Intensidad from "../assets/images/Rutinas/Intensidad.png"
-import Imagen_C2 from "../assets/images/Rutinas/ImagenC2.png"
-import Imagen_IntensidadC1 from "../assets/images/Rutinas/IntensidadC1.png"
+import TrainerImage from "../assets/images/Rutinas/Training.png"; 
 
 import MetHipertrofia from "../assets/images/Rutinas/Metodologia.png"
 
@@ -15,53 +13,46 @@ function RutinasPage() {
   return (
     <div>
       <Header/>
-        <section className='main'>
-
-          <div className='contenedor_Text_Prin_Rutinas'>
-            <p>En CEC-FGI queremos ayudarte con tus entrenamientos. Te ofrecemos varios ejemplos de programas que puedes seguir para alcanzar tus objetivos.</p>
-            <p>Es importante tener en cuenta que estos programas son solo sugerencias. Cada persona tiene condiciones físicas y mentales distintas, por lo que algunos programas pueden no ajustarse a tus necesidades. Asegúrate de conocer y respetar tus límites para evitar lesiones y otros daños.</p>
-          </div>
-          <h3 className='Titulo_Lista_Rutinas'>Entrenamiento del dia</h3>
-          <div className='contenedor_de_tabla'>
-
-              <td className='Contenido_Table'>
-                <ul className='contenido_Colum'>
-                  <li className='Fila_Principal_img'>Imagen</li>
-                  <li className='Fila_Imagen1'><img src={Imagen_C1} alt="" /></li>
-                  <li className='Fila_Secundaria'><img src={Imagen_C2} alt="" className='imagen_Circuitos' /></li>
-                  <li className='Fila_Imagen1'><img src={Imagen_C1} alt="" /></li>
-                  <li className='Fila_Secundaria'><img src={Imagen_C2} alt="" className='imagen_Circuitos'/></li>
-                </ul>
-                <ul className='contenido_Colum'>
-                  <li className='Fila_Principal'>Nombre</li>
-                  <li className='Fila_Secundaria1'>Circuito 1</li>
-                  <li className='Fila_Secundaria'>Circuito 2</li>
-                  <li className='Fila_Secundaria1'>Circuito 1</li>
-                  <li className='Fila_Secundaria'>Circuito 3</li>
-                </ul>
-                <ul className='contenido_Colum'>
-                  <li className='Fila_Principal'>Objetivo</li>
-                  <li className='Fila_Secundaria1'>Eliminar grasa / Perder peso</li>
-                  <li className='Fila_Secundaria'>Tonificar</li>
-                  <li className='Fila_Secundaria1'>Eliminar grasa / Perder peso</li>
-                  <li className='Fila_Secundaria'>Tonificar</li>
-                </ul>
-                <ul className='contenido_Colum'>
-                  <li className='Fila_Principal'>Intensidad</li>
-                  <li className='Fila_Secundaria1'><img src={Imagen_Intensidad} alt="" /></li>
-                  <li className='Fila_Secundaria'><img src={Imagen_IntensidadC1} alt="" /></li>
-                  <li className='Fila_Secundaria1'><img src={Imagen_Intensidad} alt="" /></li>
-                  <li className='Fila_Secundaria'><img src={Imagen_IntensidadC1} alt="" /></li>
-                </ul>
-                <ul className='contenido_Colum'>
-                  <li className='Fila_Principal'>Grupos Musculares</li>
-                  <li className='Fila_Secundaria1'>Piernas / Abdomen / Hombros / Trapecios</li>
-                  <li className='Fila_Secundaria'>Todos los grupos musculares</li>
-                  <li className='Fila_Secundaria1'>Piernas / Abdomen / Hombros / Trapecios</li>
-                  <li className='Fila_Secundaria'>Todos los grupos musculares</li>
-                </ul>
-              </td>
-          </div>
+      <section className="training-section">
+            <h2 className="training-title">Entrenamiento del día</h2>
+            <div className="training-content">
+                <div className="training-intro">
+                    <p>
+                        Bienvenido a la sección de entrenamientos de <strong>CEC - FGI</strong>. Aquí, encontrarás una
+                        selección personalizada de planes de entrenamiento diseñados específicamente para nuestros alumnos.
+                        Cada programa ha sido creado por nuestros expertos en fitness para ayudarte a alcanzar tus metas de
+                        salud y rendimiento.
+                    </p>
+                </div>
+                <div className="training-cards">
+                    <div className="card">
+                        <h3>Entrenamiento del día</h3>
+                        <p>
+                            Nuestros entrenamientos están adaptados a diferentes niveles de habilidad y objetivos,
+                            ya sea que estés buscando mejorar tu resistencia, ganar masa muscular, perder peso o
+                            simplemente mantenerte en forma.
+                        </p>
+                    </div>
+                    <div className="card">
+                        <h3>Seguimiento de Progreso</h3>
+                        <p>
+                            En cada entrenamiento, podrás registrar tu progreso y realizar un seguimiento de tus mejoras
+                            a lo largo del tiempo. Esta funcionalidad te permitirá mantenerte motivado y ajustar tu rutina
+                            según tus necesidades.
+                        </p>
+                    </div>
+                    <div className="card highlighted-card">
+                        <img src={TrainerImage} alt="Entrenador" className="trainer-image" />
+                        <div className="highlighted-content">
+                            <h3>Encuentra un entrenador</h3>
+                            <p>
+                                Un entrenador personal te podrá ayudar a llegar mucho más rápido a tu objetivo.
+                            </p>
+                            <button className="highlighted-button">Encontrar Entrenador</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section className='contenedor_tipos_entrenamiento'>
