b82660980dae12fbc904638624a96bfc60f7eefb
unknown <jonatanesprellalopez@gmail.com>
2024-07-01 21:13:46 -0400

    Se hizo la correccion del apartado de contactar con soporte

--- src/views/soporte.jsx
+++ src/views/soporte.jsx
@@ -16,16 +16,24 @@ function SoportePage() {
       <div className="cont-support">
         <h1>Centro de Soporte</h1>
       </div>
-      
+
       <div className="support-section">
         <h2>Ayuda y Soporte</h2>
-        <p>Encuentra respuestas a tus preguntas y obtén la ayuda que necesitas.</p>
+        <p>Bienvenido al Centro de Soporte. Aquí encontrarás respuestas detalladas a las preguntas más comunes y podrás acceder a recursos adicionales para solucionar cualquier problema que puedas tener. Nuestro equipo está aquí para asistirte en cada paso y asegurarse de que obtengas la ayuda que necesitas de manera rápida y eficiente.</p>
         
         <div className="support-buttons">
-          <button onClick={toggleFaqVisibility}>Preguntas Frecuentes (FAQ)</button>
-          <button>Contactar Soporte</button>
-          <button>Ver Tutoriales en Video</button>
-          <button>Recursos Adicionales</button>
+          <button onClick={toggleFaqVisibility}>
+            Preguntas Frecuentes (FAQ)
+            </button>
+          <button>
+            Contactar Soporte
+            </button>
+          <button>
+            Ver Tutoriales en Video
+          </button>
+          <button>
+            Recursos Adicionales
+          </button>
         </div>
         
         {faqVisible && (
@@ -39,28 +47,34 @@ function SoportePage() {
         )}
         
         <div className="contact-section">
-          <h3>Contactar Soporte</h3>
+          <h3>Contactar con Soporte</h3>
           <p>Si necesitas asistencia adicional, no dudes en contactarnos.</p>
           <div className="cont-form-support">
-          <div className="cont-img-from">
-            <img src={IMGForm} alt="" />
-          </div>
-          <form className="contact-form">
-            <label htmlFor="name">Nombre:</label>
-            <input type="text" id="name" name="name" required />
-            
-            <label htmlFor="email">Correo Electrónico:</label>
-            <input type="email" id="email" name="email" required />
-            
-            <label htmlFor="message">Mensaje:</label>
-            <textarea id="message" name="message" rows="5" required></textarea>
-            
-            <button type="submit">Enviar</button>
-          </form>
+            <div className="cont-img-from">
+              <img src={IMGForm} alt="" />
+            </div>
+
+            <form className="contact-form">
+              <label htmlFor="name">Nombre:</label>
+              <input type="text" id="name" name="name" required />
+              
+              <label htmlFor="email">Correo Electrónico:</label>
+              <input type="email" id="email" name="email" required />
+              
+              <label htmlFor="message">Mensaje:</label>
+              <textarea id="message" name="message" rows="5" required></textarea>
+              
+              <button type="submit">Enviar</button>
+            </form>
           </div>
         </div>
+
+
+
+
         
-        <div className="video-tutorials-section">
+        
+        <div>
           <h3>Tutoriales en Video</h3>
           <div className="video-container">
             <iframe 
